ba4d2d3d04cbbd023c1595cade14bb148f5164ad
Rudzainy Rahman <rudzainy@gmail.com>
2015-12-14 14:30:06 +0800

    Generate listings model & migration

---
+++ db/migrate/20151214035810_create_listings.rb
@@ -0,0 +1,11 @@
+class CreateListings < ActiveRecord::Migration
+  def change
+    create_table :listings do |t|
+    	t.integer :user_id
+    	t.string  :title
+    	t.text    :description
+
+      t.timestamps null: false
+    end
+  end
+end
